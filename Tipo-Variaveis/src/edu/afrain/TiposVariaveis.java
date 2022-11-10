d92fb3884204f98634bcaed86f1764f9b8ef051f
Afrain Calixto <calixtoafrain@gmail.com>
2022-11-10 13:22:16 -0300

    Tipos primitivos

--- Tipo-Variaveis/src/edu/afrain/TiposVariaveis.java
+++ Tipo-Variaveis/src/edu/afrain/TiposVariaveis.java
@@ -4,6 +4,19 @@ public class TiposVariaveis {
 
 	public static void main(String[] args) {
 		
+		// TIPOS PRIMITIVOS	
+		
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		numeroCurto = (short) numeroNormal;
+		
+		// CONSTANT
+		
+		final double VALOR_DE_PI = 3.14;
+		
+		
 	}
 
 }
