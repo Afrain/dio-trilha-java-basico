c21562d921b903e17119d5c2043dfba8d84e74b8
afrain <calixtoafrain@gmail.com>
2022-11-27 21:35:19 -0300

    Inseri uma linha de código a mais

--- loops-e-arrays/src/edu/afrain/exercicios/arrays/Exercicio1_OrdermInversa.java
+++ loops-e-arrays/src/edu/afrain/exercicios/arrays/Exercicio1_OrdermInversa.java
@@ -21,6 +21,7 @@ public class Exercicio1_OrdermInversa {
 		System.out.println("Vetor inverso");
 		for (int i = (vetor.length - 1); i >= 0; i--) {
 			System.out.print(vetor[i] + " ");
+			System.out.print(vetor[i] + " ");
 		}
 
 	}
